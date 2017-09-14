69a79aab36da66327dd37d62786d69f4d04a3ff5
Joseph Steinmeyer <jodalyst@mit.edu>
2017-09-14 14:21:53 -0400

    adding pi check

--- bb_runner.py
+++ bb_runner.py
@@ -31,6 +31,7 @@ hal = 'https://eesjs1.net/testsite/bb_logger'
     Edited by Joel Voldman, 2017
 '''
 
+'''
 def get_teensy_port():
     teensy_port = list(serial.tools.list_ports.grep("16c0"))
     if len(teensy_port) == 1:
@@ -49,6 +50,26 @@ def get_teensy_port():
         else:
             print("Teensy Found: Device {}".format(p))
             return port_dict[teensy_id][0].device
+'''
+
+def get_teensy_port():
+    teensy_port = list(serial.tools.list_ports.grep("16c0"))
+    if len(teensy_port) == 1:
+        print("Automatically found Teensy: {}".format(teensy_port[0]))
+        return teensy_port[0][0]
+    else:
+        ports = list(serial.tools.list_ports.comports())
+        port_dict = {i:[ports[i],ports[i].vid] for i in range(len(ports))}
+        teensy_id=None
+        for p in port_dict:
+            print("{}:   {} (Vendor ID: {})".format(p,port_dict[p][0],port_dict[p][1]))
+            if port_dict[p][1]==5824:
+                teensy_id = p
+        if teensy_id== None:
+            return False
+        else:
+            print("Teensy Found: Device {}".format(p))
+            return port_dict[teensy_id][0][0]
 
 '''
 #Then connect as usual!
