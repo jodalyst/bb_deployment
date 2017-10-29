3b7e54d4616e8b5395e615c1e76e0e7c27119d56
Joe Steinmeyer <jodalyst@mit.edu>
2017-10-29 19:38:53 -0400

    updating bb_runner to support esp32

--- bb_runner.py
+++ bb_runner.py
@@ -74,6 +74,25 @@ def get_teensy_port():
             print("Teensy Found: Device {}".format(p))
             return port_dict[teensy_id][0][0]
 
+def get_esp_port():
+    teensy_port = list(serial.tools.list_ports.grep(""))
+    if len(teensy_port) == 1:
+        print("Automatically found ESP32: {}".format(teensy_port[0]))
+        return teensy_port[0][0]
+    else:
+        ports = list(serial.tools.list_ports.comports())
+        port_dict = {i:[ports[i],ports[i].vid] for i in range(len(ports))}
+        teensy_id=None
+        for p in port_dict:
+            print("{}:   {} (Vendor ID: {})".format(p,port_dict[p][0],port_dict[p][1]))
+            if port_dict[p][1]==4292:
+                teensy_id = p
+        if teensy_id== None:
+            return False
+        else:
+            print("ESP32: Device {}".format(p))
+            return port_dict[teensy_id][0][0]
+
 '''
 #Then connect as usual!
 #I have a few different serial port threading objects you can use that are pretty robust!jj
@@ -84,9 +103,8 @@ def get_teensy_port():
 
 s = 5 #create object to hold the s serial object
 
-global s
 unique = 456
-ser = get_teensy_port()
+ser = get_esp_port()
 if not ser:
     print("No Microcontroller Found!")
 s = serial.Serial(ser) #auto-connects already I guess?
